9a10ef47116c5d6ee4665d02b25bbe3c8f675fe5
William Sawyer <williamsawyer@heidihealth.com>
2026-03-31 02:13:47 +1000

    feat: Use default DAP UI icons

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -32,19 +32,6 @@ return {
 
     dapui.setup {
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
     }
 
     dap.listeners.after.event_initialized.dapui_config = dapui.open
