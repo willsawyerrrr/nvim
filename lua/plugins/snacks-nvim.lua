164846304e47d7e5d80e44ef7accdf501bcacccb
William Sawyer <williamsawyer@heidihealth.com>
2026-04-02 20:28:21 +1100

    feat: Add more snacks

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -5,7 +5,39 @@ return {
   ---@module "snacks"
   ---@type snacks.Config
   opts = {
+    gitbrowse = { enabled = true },
     image = { enabled = true },
+    indent = { -- Pretty indent lines
+      enabled = true,
+      indent = {
+        char = '┊',
+      },
+      animate = {
+        enabled = false,
+      },
+      scope = {
+        hl = 'Comment',
+      },
+      chunk = { -- Code chunk. e.g Functions
+        enabled = true,
+        hl = 'Comment',
+        char = {
+          corner_top = '╭',
+          corner_bottom = '╰',
+
+          horizontal = '─',
+          vertical = '│',
+          arrow = '',
+        },
+      },
+    },
+    input = {
+      enabled = true,
+    },
+    notifier = {
+      enabled = true,
+      top_down = false,
+    },
     scroll = {},
   },
 }
